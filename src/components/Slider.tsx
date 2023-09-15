3c88c1c4adb44f9ff09f6b3540634b495c02c952
Jack Caldwell-Nichols <jackcaldwell-nichols@Jacks-MacBook-Pro.local>
2023-09-15 18:23:23 +0200

    changes to slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -6,29 +6,31 @@ import React, { useEffect, useState } from "react";
 const Slider = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
-  //   useEffect(() => {
-  //     const interval = setInterval(
-  //       () =>
-  //         setCurrentSlide((prev) => (prev === data.length - 1 ? 0 : prev + 1)),
-  //       3000
-  //     );
-  //     return () => clearInterval(interval);
-  //   }, []);
+  useEffect(() => {
+    const interval = setInterval(
+      () =>
+        setCurrentSlide((prev) => (prev === data.length - 1 ? 0 : prev + 1)),
+      4000
+    );
+    return () => clearInterval(interval);
+  }, []);
   const data = [
     {
       id: 1,
-      title: "always fresh & always crispy & always hot",
-      image: "/whitebg.png",
+      title: "Bringing you the best recipes...",
+      image:
+        "https://images.pexels.com/photos/1640774/pexels-photo-1640774.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
       id: 2,
-      title: "we deliver your order wherever you are in NY",
+      title: "From the exotic to the comfort...",
       image: "/bg2.png",
     },
     {
       id: 3,
-      title: "the best pizza to share with your family",
-      image: "/slide3.jpg",
+      title: "Explore the world's cuisines...",
+      image:
+        "https://images.pexels.com/photos/2641886/pexels-photo-2641886.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
   ];
   return (
@@ -38,7 +40,7 @@ const Slider = () => {
           {data[currentSlide].title}
         </h1>
       </div>
-      <div className="flex-1 relative lg:h-full" w-full>
+      <div className="flex-1 relative lg:h-full order-first" w-full>
         <Image
           src={data[currentSlide].image}
           alt=""
